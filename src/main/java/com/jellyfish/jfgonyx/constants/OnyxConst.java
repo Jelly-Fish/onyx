b3bb65d46366df26d1c04f95b84ac0c7c88e8410
thw <thw@thwcomp>
2016-03-07 21:28:28 +0100

    Move values in OnyxConst

--- src/main/java/com/jellyfish/jfgonyx/constants/OnyxConst.java
+++ src/main/java/com/jellyfish/jfgonyx/constants/OnyxConst.java
@@ -276,8 +276,8 @@ public class OnyxConst {
     
     public static enum SCORE {
     
-        WIN(100f), WIN_LINK(99.9f), TAKE(53.5f), NEIGHBOUR(1f), TAIL(1.3f),
-        RANDOM(0f), COUNTER_POS(33.6f), ATTACK(53.4f), CENTER(6.4f);
+        WIN(100f), WIN_LINK(99.9f), TAKE(53.5f), NEIGHBOUR(1f), TAIL(3.3f),
+        RANDOM(0f), COUNTER_POS(22.6f), ATTACK(23.4f), CENTER(6.4f);
         
         private final float score;
         
